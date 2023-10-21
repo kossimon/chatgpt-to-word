7814462c8704afc40e5ae8839dd3d903d96ea6c0
kossimon <simon@svetlodat.eu>
2023-10-21 18:19:15 +0200

    app update + config

--- app.py
+++ app.py
@@ -2,29 +2,48 @@ import streamlit as st
 from format import markdown_to_docx
 import re
 
-st.title('chatGPT to Microsoft Word')
+hide_streamlit_style = """
+<style>
+    #root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 1rem;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Step 3: Create UI elements
-markdown_text = st.text_area('Copy and paste and click convert:', height=500)
-convert_button = st.button('Convert to Microsoft Word')
+markdown_text = st.text_area('', height=500)
+convert_button = st.button('Vytvořit Microsoft Word')
 
 # Step 4: Handle button click event
 if convert_button:
-    if markdown_text:
-        # Get the first line of the markdown text to use as the filename
-        first_line = markdown_text.split('\n', 1)[0]
-        filename = re.sub(r'\W+', '_', first_line)
+  if markdown_text:
+    first_line = markdown_text.split('\n', 1)[0]
+    filename = re.sub(r'\W+', '_', first_line)
 
-        # Call the markdown_to_docx function to convert the input text to a DOCX file
-        docx_file_path = markdown_to_docx(markdown_text, filename)
-        
-        # Step 5: Provide a button to download the DOCX file
-        with open(docx_file_path, 'rb') as file:
-            st.download_button(
-                label="Download Word",
-                data=file,
-                file_name=f"{filename}.docx",
-                mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-            )
-    else:
-        st.warning('Please enter some Markdown text before trying to convert it.')
+    # Create a temporary directory to store the user's file
+    with tempfile.TemporaryDirectory() as tempdir:
+      # The temporary directory is automatically cleaned up when you exit this block
+
+      # Call the markdown_to_docx function, but inside the temporary directory
+      docx_file_path = markdown_to_docx(markdown_text, filename)
+
+      # Use the full path for the temporary file
+      temp_file_path = os.path.join(tempdir, docx_file_path)
+
+      # Move the generated docx to the temporary directory
+      shutil.move(docx_file_path, temp_file_path)
+
+      # Serve the file for download from the temporary directory
+      with open(temp_file_path, 'rb') as file:
+        st.download_button(
+            label="Stáhnout Microsoft Word",
+            data=file,
+            file_name=f"{filename}.docx",
+            mime=
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        )
+
+      # The temporary file and directory will be automatically deleted after serving the download
+
+  else:
+    st.warning('Please enter some Markdown text before trying to convert it.')
